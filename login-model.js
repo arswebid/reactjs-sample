defce9009cf659df57f5385ec16e108123277a05
Ars Web <128975695+arswebid@users.noreply.github.com>
2023-03-31 06:09:30 +0700

    Create login-model.js
    In this example, we are using the useState hook to keep track of the username and password values as they are entered into the input fields. When the user clicks the "Login" button, the handleSubmit function is called, which can contain your login logic. In this example, we are simply checking if the username and password match hardcoded values, but in a real-world scenario, you would typically make a request to a server to authenticate the user.

---
+++ login-model.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can add your login logic, for example:
+    if (username === 'myusername' && password === 'mypassword') {
+      alert('Login successful!');
+    } else {
+      alert('Invalid username or password.');
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
